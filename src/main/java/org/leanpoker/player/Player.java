29b3be21a7b7539b516360796ece286236fdeba7
mateusz <mateusz.glapinski@comarch.pl>
2015-07-11 10:01:21 +0200

    random

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,13 +3,14 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
+import java.util.Random;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return new Random().nextInt(100);
     }
 
     public static void showdown(JsonElement game) {
