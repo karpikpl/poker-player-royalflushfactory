8b0ba82ee7d3dc5ad0811258b2c83d8e522469a5
mateusz <mateusz.glapinski@comarch.pl>
2015-07-11 11:17:35 +0200

    sth

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,9 +10,10 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         Random random = new Random();
-		return random.nextInt(100) * random.nextInt(10);
+		return random.nextInt(100) * random.nextInt(10) + random.nextInt(100);
     }
 
     public static void showdown(JsonElement game) {
     }
+    
 }
