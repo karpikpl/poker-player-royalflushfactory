51c2dc19c0ce288409e8fbfafe51943767459ecb
Lukasz Antkowiak <lantkowiak@infusion.com>
2015-07-11 13:28:37 +0200

    RankService test

---
+++ src/test/java/org/leanpoker/rank/RankServiceTest.java
@@ -0,0 +1,35 @@
+package org.leanpoker.rank;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class RankServiceTest {
+	@Ignore
+	@Test
+	public void testCheckRank() throws Exception {
+		final List<Card> cards = new ArrayList<Card>();
+		cards.add(new Card('5', "diamonds"));
+		cards.add(new Card('6', "diamonds"));
+		cards.add(new Card('7', "diamonds"));
+		cards.add(new Card('7', "spades"));
+		cards.add(new Card('8', "diamonds"));
+		cards.add(new Card('9', "diamonds"));
+
+		final Rank rank = RankService.checkRank(cards);
+
+		final List<Integer> kickers = new ArrayList<Integer>(Arrays.asList(9, 8, 6, 5));
+
+		assertEquals(8, rank.getRank());
+		assertEquals(9, rank.getValue());
+		assertEquals(9, rank.getSecondValue());
+		assertEquals(kickers.size(), rank.getKickers().size());
+		assertTrue(kickers.containsAll(rank.getKickers()));
+	}
+}
