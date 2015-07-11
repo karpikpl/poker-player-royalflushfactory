f9a836e70e0a762d425be2310c57b8d92c6eb27c
mateusz <mateusz.glapinski@comarch.pl>
2015-07-11 13:34:29 +0200

    patient strategy

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -15,65 +15,74 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "Aggressive  Java folding player";
+	static final String VERSION = "Aggressive  Java folding player";
 
-    public static int betRequest(GameStateDto request) {
-        final int currentBuyIn = getOrElse(request.getCurrentBuyIn(), 0);
-        final int minimumRaise = getOrElse(request.getMinimumRaise(), 1);
-        final int round = getOrElse(request.getRound(), 0);
+	public static int betRequest(GameStateDto request) {
+		final int currentBuyIn = getOrElse(request.getCurrentBuyIn(), 0);
+		final int minimumRaise = getOrElse(request.getMinimumRaise(), 1);
+		final int round = getOrElse(request.getRound(), 0);
 
-        final int inAction = request.getInAction();
+		final int inAction = request.getInAction();
 
-        final PlayerDto currentPlayer = findCurrentPlayer(request, inAction);
-        int stack = getOrElse(currentPlayer.getStack(), 0);
+		final PlayerDto currentPlayer = findCurrentPlayer(request, inAction);
+		int stack = getOrElse(currentPlayer.getStack(), 0);
 
-        
-        List<HoleCardDto> hole_cards = currentPlayer.getHoleCards();
-        List<Card> cards = new ArrayList<Card>();
-        for(HoleCardDto card: hole_cards) {
-            char rank = card.getRank().charAt(0);
-            String suit = card.getSuit();
-            cards.add(new Card(rank, suit));
-        }
+		List<HoleCardDto> hole_cards = currentPlayer.getHoleCards();
+		List<Card> cards = new ArrayList<Card>();
+		for (HoleCardDto card : hole_cards) {
+			char rank = card.getRank().charAt(0);
+			String suit = card.getSuit();
+			cards.add(new Card(rank, suit));
+		}
 
-        int rank = RankService.checkRank(cards).getRank();
+		int rank = RankService.checkRank(cards).getRank();
 
-        int bet = getOrElse(currentPlayer.getBet(), 0);
+		int bet = getOrElse(currentPlayer.getBet(), 0);
 
-        int newBet = currentBuyIn - bet + minimumRaise;
-        if(rank > 0) {
-            return 1000;
-        }  else {
-            return newBet;
-        }
+		int newBet = currentBuyIn - bet + minimumRaise;
 
-    }
+		if (getPlayersInGame(request) > 2) {
+			return 0;
+		}
+		return newBet;
+	}
 
-    private static int getOrElse(JsonElement request, String property, int defaultValue) {
-        JsonElement element = request.getAsJsonObject().get(property);
-        if(element == null) {
-            return defaultValue;
-        } else {
-            return element.getAsInt();
-        }
-    }
+	private static int getPlayersInGame(GameStateDto gameState) {
+		int i = 0;
+		for (org.leanpoker.player.dto.PlayerDto player : gameState.getPlayers()) {
+			if (!player.getStatus().equals("out")) {
+				i++;
+			}
+		}
+		return i;
+	}
 
-    private static int getOrElse(Object element, int defaultValue) {
-        if(element == null) {
-            return defaultValue;
-        } else {
-            return (Integer)element;
-        }
-    }
-    private static org.leanpoker.player.dto.PlayerDto findCurrentPlayer(GameStateDto gameState, int inAction) {
-    	for (org.leanpoker.player.dto.PlayerDto player : gameState.getPlayers()) {
-            if((player.getId()!=null)&&(player.getId() == inAction)) {
-                return player;
-            }
-        }
-        throw new IllegalStateException("current player not found for given id: " + inAction);
-    }
+	private static int getOrElse(JsonElement request, String property, int defaultValue) {
+		JsonElement element = request.getAsJsonObject().get(property);
+		if (element == null) {
+			return defaultValue;
+		} else {
+			return element.getAsInt();
+		}
+	}
 
-    public static void showdown(JsonElement game) {
-    }
+	private static int getOrElse(Object element, int defaultValue) {
+		if (element == null) {
+			return defaultValue;
+		} else {
+			return (Integer) element;
+		}
+	}
+
+	private static org.leanpoker.player.dto.PlayerDto findCurrentPlayer(GameStateDto gameState, int inAction) {
+		for (org.leanpoker.player.dto.PlayerDto player : gameState.getPlayers()) {
+			if ((player.getId() != null) && (player.getId() == inAction)) {
+				return player;
+			}
+		}
+		throw new IllegalStateException("current player not found for given id: " + inAction);
+	}
+
+	public static void showdown(JsonElement game) {
+	}
 }
